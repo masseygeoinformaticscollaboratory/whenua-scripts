22f8d721988e5b91b829ba8072084c7c67c8b540
Niloofar Aflaki <naflaki4@gmail.com>
2022-07-25 16:26:26 +1200

    Allow embedding to combine all tabs

--- embedding3.py
+++ embedding3.py
@@ -152,7 +152,7 @@ def cross_validate(cache_dir, ref_keywords, k=1):
     print('TP={} FP={} TN={} FN={} Precision={} Recall={} Accuracy={} F1={}'.format(tp, fp, tn, fn, precision, recall, accuracy, f1))
 
 
-def _replace_score_with_maori_percentage_if_necessary (taumahi, rows):
+def _replace_score_with_maori_percentage_if_necessary(taumahi, rows):
     if taumahi is None:
         return rows
 
@@ -164,19 +164,24 @@ def _replace_score_with_maori_percentage_if_necessary (taumahi, rows):
     return rows
 
 
-def extract_most_similar(cache_dir, extract_type, ref_keywords, all_terms, window_size, score_type, k):
+def extract_most_similar(cache_dir, extract_type, ref_keywords, all_terms, window_size, score_type, k, combined):
     if score_type == 'maori_percentage':
         taumahi = Taumahi(verbose=False)
-        columns = ['Expression', 'geo/or not', 'Maori percentage']
+        score_column_name = 'Maori percentage'
+
     else:
         taumahi = None
-        columns = ['Expression', 'geo/or not', 'Score']
+        score_column_name = 'Score'
 
+    columns = ['Expression', 'geo/or not', score_column_name]
     if k == 1:
         columns += ['Most similar', 'Term']
 
-    output_file = 'files/embedding-{}-{}.k={}-{}.xlsx'.format(extract_type, window_size, k, score_type)
-    writer = pd.ExcelWriter(output_file, engine='xlsxwriter')
+    if combined:
+        output_file = 'files/embedding-{}-{}.k={}-{}-combined.xlsx'.format(extract_type, window_size, k, score_type)
+    else:
+        output_file = 'files/embedding-{}-{}.k={}-{}.xlsx'.format(extract_type, window_size, k, score_type)
+
     remaining_db_terms = []
     cached_terms = []
     for keyword in all_terms:
@@ -201,8 +206,10 @@ def extract_most_similar(cache_dir, extract_type, ref_keywords, all_terms, windo
             corresponding_exps.append(expr)
     reference_matrix = np.array(reference_matrix, dtype=float)
 
-    xlsx_bar = Bar('Exporting to Excel {}'.format(output_file), max=len(cached_terms) + len(remaining_db_terms))
+    ws = {}
 
+    bar = Bar('Read cached term', max=len(cached_terms),
+              suffix='%(index)d/%(max)d Elapsed %(elapsed)ds - ETA %(eta)ds')
     for keyword in cached_terms:
         if window_size == 10:
             cache_file = os.path.join(cache_dir, 'rows.{}.k={}.pkl'.format(keyword, k))
@@ -211,10 +218,11 @@ def extract_most_similar(cache_dir, extract_type, ref_keywords, all_terms, windo
         with open(cache_file, 'rb') as f:
             rows = pickle.load(f)
 
+        bar.next()
         rows = _replace_score_with_maori_percentage_if_necessary(taumahi, rows)
         df = pd.DataFrame(columns=columns, data=rows)
-        df.to_excel(writer, sheet_name=keyword, index=None)
-        xlsx_bar.next()
+        ws[keyword] = df
+    bar.finish()
 
     for keyword in remaining_db_terms:
         if window_size == 10:
@@ -228,17 +236,18 @@ def extract_most_similar(cache_dir, extract_type, ref_keywords, all_terms, windo
         for expr_ind, expr in enumerate(term_db.expressions):
             expr_full = expr.get_full_expr()
             if len(expr.avg_embbedding) == 0:
-                warning('Expression ' + expr_full + ' has nan avg embedding')
-                row = [expr_full, 'N/A', 0]
-                if k == 1:
-                    row.append('')
-                    row.append('')
-                rows.append(row)
+                # warning('Expression ' + expr_full + ' has nan avg embedding')
+                # row = [expr_full, 'N/A', 0]
+                # if k == 1:
+                #     row.append('')
+                #     row.append('')
+                # rows.append(row)
                 bar.next()
                 continue
             expr_embedding = np.array([expr.avg_embbedding], dtype=float)
             similarities = 1 - sp.distance.cdist(expr_embedding, reference_matrix, 'cosine')
             similarities = similarities.squeeze()
+
             score = None
             closest_expr = None
             is_geos = []
@@ -265,6 +274,9 @@ def extract_most_similar(cache_dir, extract_type, ref_keywords, all_terms, windo
                         is_geos.append(0)
                 score = np.mean(scores_is_geo)
 
+            if taumahi is not None:
+                _, _, _, _, score = taumahi.tiki_ōrau(expr_full)
+
             is_geos = np.array(is_geos)
             is_geo = False if len(is_geos) == 0 else is_geos.mean() >= 0.5
             row = [expr_full, 1 if is_geo else 0, score]
@@ -280,10 +292,32 @@ def extract_most_similar(cache_dir, extract_type, ref_keywords, all_terms, windo
 
         rows = _replace_score_with_maori_percentage_if_necessary(taumahi, rows)
         df = pd.DataFrame(columns=columns, data=rows)
-        df.to_excel(writer, sheet_name=keyword, index=None)
-        xlsx_bar.next()
+        ws[keyword] = df
 
-    xlsx_bar.finish()
+    writer = pd.ExcelWriter(output_file, engine='xlsxwriter')
+    if combined:
+        dfs = []
+        xlsx_bar = Bar('Processing all tabs into one tab', max=len(ws))
+        for sheet_name, df in ws.items():
+            df['Keyword'] = sheet_name
+            dfs.append(df)
+            xlsx_bar.next()
+        xlsx_bar.finish()
+
+        combined_df = pd.concat(dfs)
+        worksheets = combined_df.groupby(np.arange(len(combined_df.index)) // 1000000)
+
+        xlsx_bar = Bar('Exporting to Excel {}'.format(output_file), max=len(worksheets))
+        for (sheet_num, df) in worksheets:
+            df.to_excel(writer, sheet_name='Sheet {}'.format(sheet_num + 1), index=None)
+            xlsx_bar.next()
+        xlsx_bar.finish()
+    else:
+        xlsx_bar = Bar('Exporting to Excel {}'.format(output_file), max=len(ws))
+        for sheet_name, df in ws.items():
+            df.to_excel(writer, sheet_name=sheet_name, index=None)
+            xlsx_bar.next()
+        xlsx_bar.finish()
     writer.save()
     print('Finished extracting most similar terms and score. Output is at ' + output_file)
 
@@ -293,7 +327,14 @@ class Command(AbstractCommand):
     def __init__(self):
         super(Command, self).__init__(__file__)
 
-    def run(self, extract_type, window_size, k, score_type, recalculate_embedding):
+    def run(self, args):
+        extract_type = args.extract_type
+        window_size = args.window_size
+        k = args.k
+        score_type = args.score_type
+        recalculate_embedding = args.recalculate_embedding
+        combined = args.combined
+
         embedded_terms, embeddings = get_embeddings()
 
         embedded_terms = {term: index for index, term in enumerate(embedded_terms)}
@@ -311,35 +352,41 @@ class Command(AbstractCommand):
                   suffix='%(index)d/%(max)d Elapsed %(elapsed)ds - ETA %(eta)ds')
 
         for keyword in ref_keywords:
+            need_save = False
             term_ref = get_terms(self.cache_dir, 'ref', keyword, None)
             for exp in term_ref.expressions:
                 if exp.avg_embbedding is None or recalculate_embedding:
                     exp.calc_avg_embedding(embedded_terms, embeddings)
+                    need_save = True
             bar.next()
 
-            save_terms(self.cache_dir, 'ref', term_ref, None)
+            if need_save:
+                save_terms(self.cache_dir, 'ref', term_ref, None)
         bar.finish()
 
         bar = Bar('Calculating avg embedding for all terms', max=len(all_terms),
                   suffix='%(index)d/%(max)d Elapsed %(elapsed)ds - ETA %(eta)ds')
 
         for keyword in all_terms:
+            need_save = False
             term_db = get_terms(self.cache_dir, 'db', keyword, window_size)
             for exp in term_db.expressions:
                 if exp.avg_embbedding is None or recalculate_embedding:
                     exp.calc_avg_embedding(embedded_terms, embeddings)
+                    need_save = True
             bar.next()
 
-            save_terms(self.cache_dir, 'db', term_db, window_size)
+            if need_save:
+                save_terms(self.cache_dir, 'db', term_db, window_size)
         bar.finish()
 
         if extract_type == 'cv':
             cross_validate(self.cache_dir, ref_keywords, k)
         else:
-            extract_most_similar(self.cache_dir, extract_type, ref_keywords, all_terms, window_size, score_type, k)
+            extract_most_similar(self.cache_dir, extract_type, ref_keywords, all_terms, window_size, score_type, k, combined)
 
 
-if __name__ == '__main__':
+def main():
     parser = argparse.ArgumentParser()
     parser.add_argument('--extract-type', dest='extract_type', action='store', type=str, default='all',
                         choices=['all', 'ref', 'cv'])
@@ -348,11 +395,15 @@ if __name__ == '__main__':
     parser.add_argument('--window', dest='window_size', action='store', type=int, required=True)
     parser.add_argument('--k', dest='k', action='store', type=int, required=True)
     parser.add_argument('--recalculate-embedding', dest='recalculate_embedding', action='store_true', default=False)
+    parser.add_argument('--combined', dest='combined', action='store_true', default=False)
     args = parser.parse_args()
 
     command = Command()
     start = time.time()
-    command.run(extract_type=args.extract_type, window_size=args.window_size, k=args.k, score_type=args.score_type,
-                recalculate_embedding=args.recalculate_embedding)
+    command.run(args)
     end = time.time()
     print('Took {} seconds'.format(end - start))
+
+
+if __name__ == '__main__':
+    main()
