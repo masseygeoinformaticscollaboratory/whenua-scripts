18e8f0905b5cdeeb0beeb90ec100ec2acd0b74a7
Niloofar Aflaki <naflaki4@gmail.com>
2022-09-29 11:49:17 +1300

    Added transpose

--- calc_dist_by_group.py
+++ calc_dist_by_group.py
@@ -105,6 +105,24 @@ class Command(AbstractCommand):
         for g in groups.values():
             g.calc_avg_embedding()
 
+    @staticmethod
+    def calc_similarity_word_to_groups(word, matrix, word_group_ind):
+        matrix_excl_word_group = np.concatenate((matrix[:word_group_ind, :], matrix[word_group_ind+1:, :])).mean(axis=0)
+        between = 1 - sp.distance.cdist(word.embedding.reshape(1, -1), matrix_excl_word_group.reshape(1, -1), 'cosine')[0][0]
+        within = 1 - sp.distance.cdist(word.embedding.reshape(1, -1), matrix[word_group_ind, :].reshape(1, -1), 'cosine')[0][0]
+        return between, within
+
+    @staticmethod
+    def calc_avg_similarity_word_to_others(word_ind, matrix, group_member_indx):
+        start_ind = group_member_indx[0]
+        end_ind = group_member_indx[-1]
+        matrix_excl_word_group = np.concatenate((matrix[:start_ind, word_ind], matrix[end_ind+1:, word_ind]))
+        matrix_of_word_group = np.concatenate((matrix[start_ind:word_ind, word_ind], matrix[word_ind+1:end_ind+1, word_ind]))
+
+        between = matrix_excl_word_group.mean()
+        within = matrix_of_word_group.mean()
+        return between, within
+
     def run(self, args):
         source = args.source
         groups, all_words = self.extract_groups_and_words(source)
@@ -113,9 +131,12 @@ class Command(AbstractCommand):
         word_matrices = []
         word_vals = []
 
-        for word, w in all_words.items():
-            word_vals.append(word)
-            word_matrices.append(w.embedding)
+        for group_name, group in groups.items():
+            if len(group.words) == 0:
+                continue
+            for word in group.words:
+                word_matrices.append(word.embedding)
+                word_vals.append(word.val)
 
         word_matrices = np.array(word_matrices)
         word_dists = sp.distance.cdist(word_matrices, word_matrices, 'cosine')
@@ -133,24 +154,42 @@ class Command(AbstractCommand):
         group_dists = sp.distance.cdist(group_matrices, group_matrices, 'cosine')
         group_similarities = 1 - group_dists
 
-        writer = pd.ExcelWriter('files/group-similariries.xlsx', engine='xlsxwriter')
+        # group_ind = -1
+        # bw_wt_mat = []
+        # for group in groups.values():
+        #     if len(group.words) == 0:
+        #         continue
+        #     group_ind += 1
+        #     for word in group.words:
+        #         between, within = self.calc_similarity_word_to_groups(word, group_matrices, group_ind)
+        #         bw_wt_mat.append([between, within])
 
-        word_df = pd.DataFrame(columns=[' '] + word_vals)
-        col_ind = 0
-        for word_name, val in zip(word_vals, word_similarities):
-            row = [word_name] + list(map(float, val))
-            word_df.loc[col_ind] = row
-            col_ind += 1
+        group_ind = 0
+        bw_wt_mat = []
+        for group in groups.values():
+            if len(group.words) == 0:
+                continue
+            group_member_inds = [group_ind + x for x in range(len(group.words))]
+            for word_ind, word in enumerate(group.words):
+                between, within = self.calc_avg_similarity_word_to_others(word_ind + group_ind, word_similarities, group_member_inds)
+                bw_wt_mat.append([between, within])
 
+            group_ind += len(group.words)
+
+        bw_wt_mat = np.array(bw_wt_mat, dtype=float)
+
+        writer = pd.ExcelWriter('files/group-similariries-bw-wt.xlsx', engine='xlsxwriter')
+
+        bw_wt_data = np.concatenate((np.array(word_vals, dtype='O').reshape(-1, 1), bw_wt_mat), axis=1)
+        bw_wt_df = pd.DataFrame(columns=['', 'Between', 'Within'], data=bw_wt_data)
+        bw_wt_df.to_excel(writer, sheet_name='Between, within', index=None)
+
+        word_data = np.concatenate((np.array(word_vals, dtype='O').reshape(-1, 1), word_similarities), axis=1)
+        word_df = pd.DataFrame(columns=[' '] + word_vals, data=word_data)
         word_df.to_excel(writer, sheet_name='Word similarities', index=None)
 
-        group_df = pd.DataFrame(columns=[' '] + group_vals)
-        col_ind = 0
-        for group_name, val in zip(group_vals, group_similarities):
-            row = [group_name] + list(map(float, val))
-            group_df.loc[col_ind] = row
-            col_ind += 1
-
+        group_data = np.concatenate((np.array(group_vals, dtype='O').reshape(-1, 1), group_similarities), axis=1)
+        group_df = pd.DataFrame(columns=[' '] + group_vals, data=group_data)
         group_df.to_excel(writer, sheet_name='Group similarities', index=None)
 
         writer.save()
